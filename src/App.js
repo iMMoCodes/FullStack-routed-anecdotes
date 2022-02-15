e307858a98f64503593efb8ffaae80c778be24f9
iMMoCodes <matti-makela@hotmail.com>
2022-02-15 02:34:49 +0200

    Tehtävä 7.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -25,11 +31,30 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
 )
+
+const SingleAnecdote = ({ anecdotes }) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find((n) => n.id === Number(id))
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>has {anecdote.votes} votes</div>
+      <div>
+        for more info see &nbsp;
+        <a target='_blank' rel='noreferrer' href={anecdote.info}>
+          {anecdote.info}
+        </a>
+      </div>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -156,9 +181,13 @@ const App = () => {
       <h1>Software anecdotes</h1>
       <Menu />
       <Routes>
-        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route
+          path='/anecdotes/:id'
+          element={<SingleAnecdote anecdotes={anecdotes} />}
+        />
         <Route path='/about' element={<About />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
       </Routes>
       <Footer />
     </Router>
